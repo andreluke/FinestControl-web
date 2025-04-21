e3470d1d81dce2880a557ae0500cdd71af9c9cdf
andreluke <andre.l.sales2706@gmail.com>
2025-04-21 19:45:00 -0300

    :bricks: build: Melhoria do next

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
+import path from 'node:path'
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  webpack: config => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    return config
+  },
+}
 
 export default nextConfig
